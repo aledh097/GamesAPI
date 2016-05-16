90e16b6e1d8aa140efd70a51a1b0aa03f99ca409
alex <muchoh123@hotmail.com>
2016-05-16 17:19:10 +0200

    comentario

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -35,14 +35,16 @@ def perfil():
 def clan():
     return template('clan.tpl')
 
-@route('/resultadoperfil')
+@route('/resultadoperfil',method='POST')
 def resultadoperfil():
 	id_perfil = request.forms.get("idperfil")
 	dicc_parametros = {'key':'1685786EECBF130267010877BAB447D0','steamids':id_perfil}
-    r=requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params=dicc_parametros)
-    datos=json.loads(r.text.encode("utf-8"))
-    id=datos["response"]["players"][0]["steamid"]
-    nick=datos["response"]["players"][0]["personaname"]
+    r = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params=dicc_parametros)
+    datos = json.loads(r.text.encode("utf-8"))
+    try:
+    	id=datos["response"]["players"][0]["steamid"]
+    try:
+    	nick=datos["response"]["players"][0]["personaname"]
     return template('resultadoperfil.tpl')
 
 # This must be added in order to do correct path lookups for the views
