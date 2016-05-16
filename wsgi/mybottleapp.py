8b34c7f0ab7a23fe43f8af2e8e6b44024079d249
alex <muchoh123@hotmail.com>
2016-05-16 17:55:14 +0200

    comentario

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -31,7 +31,7 @@ def twitter():
 def clan():
     return template('clan.tpl')
 
-@post('/perfil')
+@bottle.post('/perfil')
 def perfil():
     id_perfil = request.forms.get("idperfil")
     dicc_parametros={'key':'1685786EECBF130267010877BAB447D0','steamids':id_perfil}
@@ -41,7 +41,7 @@ def perfil():
     nick=datos["response"]["players"][0]["personaname"]
     return template('resultadoperfil.tpl')
 
-@get('/resultadoperfil')
+@bottle.get('/resultadoperfil')
 def resultadoperfil():
     return template('perfil.tpl')
 
