5b50d4ee7dd43243e3f0d2e3a5b463ff1a80d202
alex <muchoh123@hotmail.com>
2016-05-16 20:03:13 +0200

    comentario

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -41,14 +41,8 @@ def resultadoperfil():
     dicc_parametros={'key':'1685786EECBF130267010877BAB447D0','steamids':id_perfil}
     r = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params=dicc_parametros)
     datos = json.loads(r.text.encode("utf-8"))
-    try:
-        id=datos["response"]["players"][0]["steamid"]
-    else:
-        id="no disponible"
-    try:
-        nick=datos["response"]["players"][0]["personaname"]
-    else:
-        nick="no disponible"
+    id=datos["response"]["players"][0]["steamid"]
+    nick=datos["response"]["players"][0]["personaname"]
     return template('resultadoperfil.tpl',id_perfil=id_perfil,id=id,nick=nick)
 
 # This must be added in order to do correct path lookups for the views
