b85c05c2b828ea4f440281e60637c3030e56580d
alex <muchoh123@hotmail.com>
2016-05-16 22:33:51 +0200

    comentario

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -3,6 +3,7 @@ import urllib2
 import requests
 import json
 import time
+import os
 
 @route('/name/<name>')
 def nameindex(name='Stranger'):
@@ -57,9 +58,6 @@ def resultadoperfil():
 
         return template('resultadoperfil.tpl',id_perfil=id_perfil,id=id,nick=nick,nombre_real=nombre_real,Avatar=Avatar,Pais=Pais,Codigo_del_Estado=Codigo_del_Estado,Codigo_de_la_ciudad=Codigo_de_la_ciudad,estado=estado, ultimo_logeo=ultimo_logeo,id_clan=id_clan,fecha_creacion=fecha_creacion)
 
-# This must be added in order to do correct path lookups for the views
-import os
-from bottle import TEMPLATE_PATH
 TEMPLATE_PATH.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi/views/')) 
 
 application=default_app()
