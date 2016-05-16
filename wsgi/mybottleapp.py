4415a808762914416b89f952c94de735f3a2b3fb
alex <muchoh123@hotmail.com>
2016-05-16 17:09:10 +0200

    comentario

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -38,14 +38,14 @@ def clan():
 @route('/resultadoperfil')
 def resultadoperfil():
 	id_perfil = request.forms.get("idperfil")
-	#dicc_parametros = {'key':'1685786EECBF130267010877BAB447D0','format':'json','steamids':id_perfil}
-    #r = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params=dicc_parametros)
-    #datos = json.loads(r.text.encode("utf-8"))
-    #try:
-    #	id=datos["response"]["players"][0]["steamid"]
-    #try:
-    #	nick=datos["response"]["players"][0]["personaname"]
-    #return template('resultadoperfil.tpl')
+	dicc_parametros = {'key':'1685786EECBF130267010877BAB447D0','format':'json','steamids':id_perfil}
+    r = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params=dicc_parametros)
+    datos = json.loads(r.text.encode("utf-8"))
+    try:
+    	id=datos["response"]["players"][0]["steamid"]
+    try:
+    	nick=datos["response"]["players"][0]["personaname"]
+    return template('resultadoperfil.tpl')
 
 # This must be added in order to do correct path lookups for the views
 import os
