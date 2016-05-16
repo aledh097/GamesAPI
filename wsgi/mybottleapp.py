00d53739bbd389979600644a4522a34328bf320e
alex <muchoh123@hotmail.com>
2016-05-16 18:17:29 +0200

    comentario

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -2,7 +2,6 @@ from bottle import route, run, template, get, post, request, response, redirect,
 import urllib2
 import requests
 
-HOST = '87.221.173.76'
 
 @route('/name/<name>')
 def nameindex(name='Stranger'):
