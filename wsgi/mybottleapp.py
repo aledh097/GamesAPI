4fc707f569769c807e4d7361547787bd4e641735
alex <muchoh123@hotmail.com>
2016-05-16 22:51:40 +0200

    comentario

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -42,7 +42,6 @@ def resultadoperfil():
     dicc_parametros={'key':'1685786EECBF130267010877BAB447D0','steamids':id_perfil}
     r = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params=dicc_parametros)
     datos = json.loads(r.text.encode("utf-8"))
-try:
     nombre_real= datos["response"]["players"][0]["realname"]
     id=datos["response"]["players"][0]["steamid"]
     nick=datos["response"]["players"][0]["personaname"]
