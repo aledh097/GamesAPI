8026afa574720c861a3c7cea29c10a1fa2d344d6
alex <muchoh123@hotmail.com>
2016-05-29 15:44:23 +0200

    comentario

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -218,7 +218,7 @@ def resultadoranking():
     datos5 = json.loads(r6.text.encode("utf-8"))
     total_victimas=datos5["playerstats"]["stats"][0]["value"]
     total_muertes=datos5["playerstats"]["stats"][1]["value"]
-    tiempo=time.ctime(int(datos5["playerstats"]["stats"][2]["value"]))
+    tiempo=time.strftime("%H", time.localtime(int("datos5[playerstats"]["stats"][2]["value"])))
     return template('resultadoranking.tpl', nick=nick,total_victimas=total_victimas,total_muertes=total_muertes,tiempo=tiempo)
 
 def get_request_token():
