719df7815e7610fc002b93ed4aacf87babf8dd43
alex <muchoh123@hotmail.com>
2016-05-31 20:09:08 +0200

    error 404

--- wsgi/mybottleapp.py
+++ wsgi/mybottleapp.py
@@ -130,14 +130,60 @@ def resultadoperfil2():
     nick = request.forms.get("nick")
     dicc_parametros2={'key':'1685786EECBF130267010877BAB447D0','vanityurl':nick}
     r2 = requests.get("http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/", params=dicc_parametros2)
-    datosnick = json.loads(r2.text.encode("utf-8"))
-    id_nick=datosnick["response"]["steamid"]
+    datos2 = json.loads(r2.text.encode("utf-8"))
+    id_nick=datos2["response"]["steamid"]
     dicc_parametros3={'key':'1685786EECBF130267010877BAB447D0','steamids':id_nick}
     r3 = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params=dicc_parametros3)
     datos2 = json.loads(r3.text.encode("utf-8"))
-    
-    if datos2 == false:
-        return template('404.tpl', nick=nick)
+    try:
+        nombre_real= datos2["response"]["players"][0]["realname"]    
+    except:
+        nombre_real="no disponible"
+    try:
+        id= datos2["response"]["players"][0]["steamid"]    
+    except:
+        id="no disponible"
+    try:
+        nick=datos2["response"]["players"][0]["personaname"]   
+    except:
+        nick="no disponible"
+    try:
+        Avatar=datos2["response"]["players"][0]["avatarfull"]
+    except:
+        Avatar="no disponible"
+    try:
+        Pais=datos2["response"]["players"][0]["loccountrycode"]
+    except:
+        Pais="no disponible"
+    try:
+        Codigo_del_Estado=datos2["response"]["players"][0]["locstatecode"]
+    except:
+        Codigo_del_Estado="no disponible"
+    try:
+        Codigo_de_la_ciudad=datos2["response"]["players"][0]["loccityid"]
+    except:
+        Codigo_de_la_ciudad="no disponible"
+    try:
+        estado=datos2["response"]["players"][0]["personastate"]
+    except:
+        estado="no disponible"
+    try:
+        ultimo_logeo=time.ctime(int(datos2["response"]["players"][0]["lastlogoff"]))
+    except:
+        ultimo_logeo="no disponible"
+    try:
+        id_clan=datos2["response"]["players"][0]["primaryclanid"]
+    except:
+        id_clan="no disponible"
+    try:
+        fecha_creacion=time.ctime(int(datos2["response"]["players"][0]["timecreated"]))
+    except:
+        fecha_creacion="no disponible"
+    get_request_token()
+    authorize_url = AUTHENTICATE_URL + TOKENS["request_token"]
+    response.set_cookie("request_token", TOKENS["request_token"],secret='some-secret-key')
+    response.set_cookie("request_token_secret", TOKENS["request_token_secret"],secret='some-secret-key')
+    return template('resultadoperfil2.tpl',id_perfil=id_nick,id=id,nick=nick,nombre_real=nombre_real,Avatar=Avatar,Pais=Pais,Codigo_del_Estado=Codigo_del_Estado,Codigo_de_la_ciudad=Codigo_de_la_ciudad,estado=estado,ultimo_logeo=ultimo_logeo,id_clan=id_clan,fecha_creacion=fecha_creacion,authorize_url=authorize_url)
 
 @route('/resultadonoticia', method='POST')
 def resultadonoticia():
@@ -164,7 +210,7 @@ def resultadoestadisticas():
     datos4 = json.loads(r5.text.encode("utf-8"))
     id_nick=datos4["response"]["steamid"]
     dicc_parametros6={'key':'1685786EECBF130267010877BAB447D0','steamid':id_nick}
-    r6 = requests.get("http://api.steam powered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730", params=dicc_parametros6)
+    r6 = requests.get("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=730", params=dicc_parametros6)
     datos5 = json.loads(r6.text.encode("utf-8"))
     dicc_parametros7={'key':'1685786EECBF130267010877BAB447D0','steamids':id_nick}
     r7 = requests.get("http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/", params=dicc_parametros7)
